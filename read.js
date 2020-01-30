180e45ee7400009fe3f9331327cc00798efdc2a8
Geovanna da Silva Mendes <geovannadasilvamendes@hotmail.com>
2020-01-30 11:09:26 -0300

    read.js

--- read.js
+++ read.js
@@ -1,25 +1,72 @@
-require('./models/user');
-require('./config/connection.js');
-var userService = require('./service/user');
+var assert = require("assert");
 
-//
+//model
+var mongoose = require('mongoose');
+var schema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        default: ''
+    },
+    lastName: {
+        type: String,
+        default: ''
+    },
+    dob: Date,
+    address: String,
+    phone: String,
+    role: String
+});
+var user = new mongoose.model('User', schema);
+//model
+
+
+//config
+//var user = mongoose.model('User');
+var url = 'mongodb://localhost/crud01';
+
+// Make Mongoose use `findOneAndUpdate()`. 
+// Note that this option is `true` by default,
+// you need to set it to false.
+//mongoose.set('useFindAndModify', false);
+//mongoose.set('useCreateIndex', true);
+
+// Connection establishment
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+//var db = mongoose.connection;
+//config
+
+
 // read
-//
 var query = {
     username: "abc@example.com"
 };
 
+var service = {
+    findUser: function (query, callback) {
+        return user.findOne(query, callback);
+    },
+};
+//console.log(service);
 
-userService.findUser(query, function (error, response) {
+var bar = service.findUser(query, function (error, response) {
     if (error) {
         console.log(error);
-        return;
-    }
-    if (response) {
-        console.log(response);
         return;
     }
     if (!response) {
         console.log('No Data Found');
     }
+    assert.equal("abc@example.com", response.username);
+    console.log("passou!!");
+    return;
 });
+//read
